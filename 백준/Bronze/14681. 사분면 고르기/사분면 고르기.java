9f26ddbf4acae2b35fb2274f67215210cba05745
Songhyun Go <zhthd201@gmail.com>
2026-01-28 12:29:35 +0900

    [Bronze V] Title: 사분면 고르기, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if (a > 0 && b > 0) {
+            System.out.printf("1");
+        } else if (a < 0 && b > 0) {
+            System.out.printf("2");
+        } else if (a < 0 && b < 0) {
+            System.out.printf("3");
+        } else if (a > 0 && b < 0) {
+            System.out.printf("4");
+        }
+    }
+}
