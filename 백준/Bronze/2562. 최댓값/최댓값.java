52133970db7774292413271322fd928913d2c1f1
Song <zhthd201@gmail.com>
2026-03-21 15:45:53 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = 0;
+        int index = 0;
+
+        for (int i = 1; i <= 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num > max) {
+                max = num;
+                index = i;
+            }
+
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
