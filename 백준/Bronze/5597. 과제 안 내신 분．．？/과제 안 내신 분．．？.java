cd484c2469e87affe1962f6353674b3f9e03027e
Song <zhthd201@gmail.com>
2026-03-22 21:22:34 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] submitted = new boolean[31];
+
+        for (int i = 0; i < 28; i++) {
+            int n = Integer.parseInt(br.readLine());
+            submitted[n] = true;
+        }
+
+        for (int i = 1; i <= 30; i++) {
+            if (!submitted[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
