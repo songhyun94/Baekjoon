c493507f49ae662bcb887881cf82d8b378c648b6
Song <zhthd201@gmail.com>
2026-03-30 16:42:35 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 116 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+        String[] croatia = {
+                "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="
+        };
+
+        for (int i = 0; i < croatia.length; i++) {
+            word = word.replace(croatia[i], "*");
+        }
+
+        System.out.println(word.length());
+    }
+}
